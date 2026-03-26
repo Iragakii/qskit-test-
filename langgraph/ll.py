dae635dd85ce910cfa22c73d50b98479033631ca
Iragaki JC <jcgroup.vni@gmail.com>
2026-03-26 19:19:14 +0700

    haiz

--- langgraph/ll.py
+++ langgraph/ll.py
@@ -11,41 +11,35 @@ class AgentState(TypedDict) :
 
 llm = ChatOllama(model = "llama3")
 
-def process(state : AgentState) -> AgentState : 
-    "This node will solve the request u input "
+def process(state : AgentState) -> AgentState :
     response = llm.invoke(state["messages"])
-    state["messages"].append(AIMessage(content = response.content))
+    state["messages"].append(AIMessage(content=response.content))
+    print(f"\n AI :  {response.content}" )
 
-    print(f"\nAI : {response.content}" )
     return state 
 
 graph = StateGraph(AgentState)
 graph.add_node("process_node" , process)
-graph.add_edge(START  , "process_node")
-graph.add_edge("process_node" , END)
+graph.add_edge(START , "process_node")
+graph.add_edge("process_node" ,END)
 agent = graph.compile()
 
 conversation_history = []
+user_input = input("Enter :")
 
-user_input = input("Enter : ")
-
-while user_input != "exit" : 
+while user_input != "exit" :
     conversation_history.append(HumanMessage(content=user_input))
     result = agent.invoke({"messages" : conversation_history})
     conversation_history = result["messages"]
-    user_input = input("Enter : ")
-with open("logging.txt " , "w") as file : 
-    file.write("Your conversation log : \n")
 
-    for messages in conversation_history : 
+    user_input = input("Enter :")
+with open("logging.txt" , "w") as file :
+    file.write("Chat log")
+    for messages in conversation_history :
         if isinstance(messages , HumanMessage) : 
-            file.write(f"You :{messages.content} \n ")
-        elif isinstance(messages , AIMessage) :
-            file.write(f"AI :  {messages.content} \n \n" )
-    file.write("End Of Conversation")
+            file.write(f"You : {messages.content} \n")
+        elif isinstance(messages , AIMessage) : 
+            file.write(f"AI : {messages.content} \n \n")
+    file.write("END")
 
-print("Conversation saved to logging.txt")    
-
-
-
-
+print("conversation loggin.txt")
