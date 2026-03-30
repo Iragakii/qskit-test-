0fe17f2d75e98b443b5e200ead01f0a1d002259c
Iragaki JC <jcgroup.vni@gmail.com>
2026-03-30 22:22:13 +0700

    fa

--- langgraph/ll.py
+++ langgraph/ll.py
@@ -4,11 +4,10 @@ from langchain_core.messages import HumanMessage , AIMessage
 from langchain_community.chat_models import ChatOllama
 from langgraph.graph import StateGraph , START , END 
 
-
-class AgentState(TypedDict) : 
+class AgentState(TypedDict): 
     messages : List[Union[HumanMessage , AIMessage]]
 
-llm = ChatOllama(model ="llama3")
+llm = ChatOllama(model = "llama3")
 
 def process(state : AgentState) -> AgentState : 
     response = llm.invoke(state["messages"])
@@ -20,27 +19,27 @@ def process(state : AgentState) -> AgentState :
 graph = StateGraph(AgentState)
 graph.add_node("process_node" , process)
 graph.add_edge(START , "process_node")
-graph.add_edge("process_node" ,END)
+graph.add_edge("process_node" , END)
 
 agent = graph.compile()
 
 conversation_history = []
 users_input = input("Enter :")
 
-while users_input != "exit": 
+while users_input != "exit":
     conversation_history.append(HumanMessage(content = users_input))
     result = agent.invoke({"messages" : conversation_history})
     conversation_history = result["messages"]
+    
+    users_input = input("Enter :")
 
-    users_input = input("Enter:")
-
-with open("longging.txt" , "w") as file:
+with open("longging.txt" , "w") as file :
     file.write("Chat History")
-
-    for messages in conversation_history: 
-        if isinstance(messages , HumanMessage): 
+    for messages in conversation_history:
+        if isinstance(messages , HumanMessage):
             file.write(f"You : {messages.content} \n")
-        elif isinstance(messages , AIMessage): 
-            file.write(f"AI : {messages.content}")
-    file.write("End Chat")
-print("saved")
+        if isinstance(messages, AIMessage):
+            file.write(f"AI : {messages.content} \n \n")
+    file.write("End Chat ")
+
+print("Print")
